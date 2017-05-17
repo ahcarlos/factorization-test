8dddfaaef9fd3f1802fc56d00d3ecb3c9b6f972c
Carlos de Armas <alu0100816167@ull.edu.es>
2017-05-17 10:44:19 +0100

    Añadiendo nuevo test

---
+++ fermat.rb
@@ -0,0 +1,22 @@
+def perfect_suqare?(input)
+  closest = Math.sqrt(input).ceil
+  return true if input == closest * closest
+end
+
+def fermat(num)
+  a = Math.sqrt(num).ceil
+  b2 = a**2 - num
+
+  loop do
+    a += 1
+    b2 = a**2 - num
+    break if perfect_square?(b2)
+  end
+
+  fac1 = a - Math.sqrt(b2)
+  fac2 = a + Math.sqrt(b2)
+  [fac1.round, fac2.round]
+end
+
+# r = fermat(5959)
+# p r1
